a39eb097c42e2176c16958bec63da462f19cccf7
Boris <36640907+B0RlS@users.noreply.github.com>
2019-05-29 17:58:00 +0300

    [Features] Registration / authorization / memorization (#7)
    * Add Users and Home controllers
    
    * Add User model with validation
    
    * Add regex in User model
    
    * Update User Controller and Add some views
    
    * Add Sessions
    
    * Changed registration and authorization views
    
    * Fix bag in User model
    
    * Added memorization. Version 1.0
    
    * Add remember and forget user features
    
    * Add validation specs
    
    * Add Users controller specs
    
    * Add Sessions and helpers specs
    
    * Deleted empty files
    
    * Deleted empty files
    
    * Deleted empty files, fix bag in SessionController tests
    
    * Add module Authenticatable
    
    * Deleted empty files
    
    * Fix rubocop errors
    
    * Fix bag in session helper and rubocop errors

--- app/mailers/application_mailer.rb
+++ app/mailers/application_mailer.rb
@@ -1,3 +1,6 @@
+# frozen_string_literal: true
+
+# Mailer for project
 class ApplicationMailer < ActionMailer::Base
   default from: 'from@example.com'
   layout 'mailer'
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,12 @@
+# frozen_string_literal: true
+
 Rails.application.routes.draw do
+  root               'home#home'
+  get '/signup', to: 'users#new'
+  get '/login',  to: 'sessions#new'
+  post '/login', to: 'sessions#create'
+  delete '/logout', to: 'sessions#destroy'
+
+  resources :users
   resources :profiles, except: [:index]
 end

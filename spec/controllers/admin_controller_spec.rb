ad15aa10b88a4f02d0b98aa875c1726dfb2dad03
Vasili Makouchyk <vmakovchik@gmail.com>
2019-05-31 19:39:53 +0300

    Add rubocop config

--- spec/controllers/admin_controller_spec.rb
+++ spec/controllers/admin_controller_spec.rb
@@ -3,4 +3,22 @@
 require 'rails_helper'
 
 RSpec.describe AdminController, type: :controller do
+  let(:admin) { User.create(valid_params) }
+  let(:valid_params) do
+    {
+      name: 'Boris',
+      surname: 'Tsarikov',
+      email: 'example@example.com',
+      password: '1234567',
+      role: :admin
+    }
+  end
+  let(:invalid_params) do
+    {
+      name: nil,
+      surname: nil,
+      email: nil,
+      password: nil
+    }
+  end
 end

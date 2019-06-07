6c2edb7b305b48a4cd71483a9bac77b44fa6998b
nikitathebest <nikita.m.7007@gmail.com>
2019-06-07 17:39:23 +0300

    Delete unnecessary comments

--- app/controllers/educations_controller.rb
+++ app/controllers/educations_controller.rb
@@ -1,31 +1,11 @@
 class EducationsController < ApplicationController
-  # def show
-  #   @education = Education.find(params[:id])
-  # end
-
-  def new
-    @education = Education.new
-  end
 
   def edit
     @profile = Profile.find(params[:user_id])
     authorize @profile
-    # @education = Education.find(params[:id])
     @user = current_user
-    # @education = Education.where(user_id: @user.id)
     @education = current_user.education
-    # @education = Education.find(params[:user_id])
   end
-
-  # def create
-  #   @education = Education.new(education_params)
-   
-  #   if @education.save
-  #     redirect_to @education
-  #   else
-  #     render :new
-  #   end
-  # end
 
   def update
     @profile = Profile.find(params[:user_id])

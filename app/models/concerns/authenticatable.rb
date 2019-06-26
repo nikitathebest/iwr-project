fc62c1dcb57d3c0a2a20a3696efd4e3d7bc48896
Boris <36640907+B0RlS@users.noreply.github.com>
2019-06-26 09:29:29 +0300

    Fix bug in BCrypt user.digest method (#50)

--- app/models/concerns/authenticatable.rb
+++ app/models/concerns/authenticatable.rb
@@ -4,12 +4,7 @@ module Authenticatable
   include ActiveSupport::Concern
   # Returns the hash digest of the given string.
   def User.digest(string)
-    cost = if BCrypt::Engine::MIN_COST
-             ActiveModel::SecurePassword.min_cost
-           else
-             BCrypt::Engine.cost
-           end
-    BCrypt::Password.create(string, cost: cost)
+    BCrypt::Password.create(string)
   end
 
   def User.new_token
